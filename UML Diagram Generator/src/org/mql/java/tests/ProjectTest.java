25464d88b89807d2e7f8f292f96113f6987a5363
nazih-amri <heyynazih@gmail.com>
2024-01-19 17:57:44 +0100

    Created unit tests using JUnit 5

---
+++ UML Diagram Generator/src/org/mql/java/tests/ProjectTest.java
@@ -0,0 +1,40 @@
+package org.mql.java.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.mql.java.models.ClassModel;
+import org.mql.java.models.ProjectModel;
+import org.mql.java.reflection.ProjectExplorer;
+
+class ProjectTest {
+
+	@Test
+	void testProjectNotNull() {
+		File file = new File("C:\\Users\\na-zi\\git\\repository3\\UML Diagram Generator");
+		ProjectExplorer projectExplorer = new ProjectExplorer(file);
+		ProjectModel project = projectExplorer.parse();
+		assertTrue(project!=null);
+	}
+	
+	@Test
+	void testProjectPackagesNotNull() {
+		File file = new File("C:\\Users\\na-zi\\git\\repository3\\UML Diagram Generator");
+		ProjectExplorer projectExplorer = new ProjectExplorer(file);
+		ProjectModel project = projectExplorer.parse();
+		assertTrue(project.getPackages().size()>0);
+	}
+	
+	@Test
+	void testProjectName() {
+		File file = new File("C:\\Users\\na-zi\\git\\repository3\\UML Diagram Generator");
+		ProjectExplorer projectExplorer = new ProjectExplorer(file);
+		ProjectModel project = projectExplorer.parse();
+		assertEquals("UML Diagram Generator",project.getName());
+	}
+	
+}
