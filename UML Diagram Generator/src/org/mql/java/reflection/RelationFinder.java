cf099e4a5b732272326ca467320dbdfef283c670
nazih-amri <heyynazih@gmail.com>
2024-01-20 19:47:56 +0100

    Verified RelationFinder results using FileLogger.

--- UML Diagram Generator/src/org/mql/java/reflection/RelationFinder.java
+++ UML Diagram Generator/src/org/mql/java/reflection/RelationFinder.java
@@ -4,6 +4,8 @@ import org.mql.java.models.RelationModel;
 import java.util.List;
 import java.util.Optional;
 import java.util.Vector;
+import org.mql.java.loggers.FileLogger;
+import org.mql.java.loggers.Logger;
 import org.mql.java.models.ClassModel;
 import org.mql.java.models.FieldModel;
 import org.mql.java.models.InterfaceModel;
@@ -17,6 +19,8 @@ public class RelationFinder {
 
 	public static List<RelationModel> find(List<ClassModel> classes) {
 		List<RelationModel> relations = new Vector<>();
+		Logger logger = new FileLogger("resources/logs/relations.log");
+		logger.log("Project Relations", "");
 		for (ClassModel source : classes) {
 			relations.addAll(getImplemetations(source));
 			for (ClassModel target : classes) {
@@ -25,6 +29,9 @@ public class RelationFinder {
 					Optional.ofNullable(getAssociation(source, target)).ifPresent(relations::add);
 				}
 			}
+		}
+		for (RelationModel r : relations) {
+			logger.log("Relation", r.getName() + "--->" + ", source: " + r.getSource() + " , target : " + r.getTarget());
 		}
 		return relations;
 	}
